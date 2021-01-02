65f7d15ce82447284f54955efc9ff4503b5a31ef
krishnadevz <krishnakakade77@gmail.com>
2021-01-02 16:30:10 +0530

    second component changes

--- src/components/Space.js
+++ src/components/Space.js
@@ -1,8 +1,25 @@
 import React from "react";
+import {useState,useEffect} from "react";
 function Space() {
+  const [photoData,setPhotoData]=useState(null);
+  useEffect(()=>{
+fetchPhoto();
+async function fetchPhoto(){
+  const res = await fetch(`https://api.nasa.gov/planetary/apod?api_key=hETQq0FPsZJnUP9C3sUEFtwmJH3edb4I5bghfWDM`);
+  const data=await res.json();
+  setPhotoData(data);
+}
+  },[]); //empty array for running only once then empty array for that 
+ 
+ 
+  if (!photoData) return <div />;
   return (
     <div className="Space">
-      <h1>hello world</h1>
+      <img
+      src={photoData.url}
+      alt={photoData.title}
+      className="photo"
+      />
     </div>
   );
 }
