ab7f07d1905b1b16c159d1a85a986a51da5a03f3
krishnadevz <krishnakakade77@gmail.com>
2021-01-03 16:39:59 +0530

    fetch stuff and aligned the explantion and stuff but images are still not responsive

--- src/components/Space.js
+++ src/components/Space.js
@@ -1,4 +1,6 @@
 import React from "react";
+import styles from "./Space.module.css";
+
 import {useState,useEffect} from "react";
 function Space() {
   const [photoData,setPhotoData]=useState(null);
@@ -12,22 +14,37 @@ async function fetchPhoto(){
 }
   },[]); //empty array for running only once then empty array for that 
  
- 
+
   if (!photoData) return <div />;
+  
   return (
-    <div className="Space">
-      <img
-      src={photoData.url}
-      alt={photoData.title}
-      className="photo"
-      />
+    <>
+    <div className={styles.space}>
+      {photoData.media_type === "image" ? (
+        <img 
+          src={photoData.url}
+          alt={photoData.title}
+          className={styles.space}
+        />
+      ) : (
+        <iframe
+          title="space-video"
+          src={photoData.url}
+          frameBorder="0"
+          gesture="media"
+          allow="encrypted-media"
+          allowFullScreen
+          className={styles.space}
+        />
+      )}
       <div>
         <h1>{photoData.title}</h1>
-        <p>{photoData.date}</p>
-        <p>{photoData.explanation}</p>
+        <p className={styles.space.date}>{photoData.date}</p>
+        <p className={styles.space.explanation}>{photoData.explanation}</p>
       </div>
+      
     </div>
+    </>
   );
 }
-
 export default Space;
