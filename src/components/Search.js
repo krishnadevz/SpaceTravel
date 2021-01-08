71720467e82604d0275c9eb18a7d8e69ea2874ee
krishnadevz <krishnakakade77@gmail.com>
2021-01-08 07:27:27 +0530

    3rd component changes

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,24 +1,58 @@
-import React, {useState,useEffect} from "react"
-
+import React, {useState} from "react"
+import Unsplash, { toJson } from "unsplash-js";
 function Search() {
+  const [query, setQuery] = useState("");
+  const unsplash = new Unsplash({
+    accessKey: "Xhyev2ttVhtveYdQhiRjp4WT1Pe_69kQI4hWOUn81y8",
+  });
+  const [pics, setPics] = useState([]);
 
-  const [ keyword, setKeyword] = useState('');
-  const [photoData,setPhotoData]=useState(null);
-  useEffect(()=>{
-    fetchPhoto();
-    async function fetchPhoto(){
-      const res = await fetch(`http://images-api.nasa.gov/search?q=sun`); //i am trying to build here something like when we type 
-      // sun then sun image will be shown can you work on this https://www.youtube.com/watch?v=GiIQce7Rx4Y&t=647s
-      const data=await res.json();
-      setPhotoData(data);
-      console.log(data);
-    }
-  },[]);
-  
+ 
+const searchPhotos = async (e) => {
+  e.preventDefault();
+  unsplash.search
+      .photos(query, 1, 20)
+      .then(toJson)
+      .then((json) => {
+        setPics(json.results);
+  });
+};
   return (
-<h1>hello</h1>
-  )
-
+    <>
+    <div className="container">
+        <h1 className="title">Space Photo Search</h1>
+      </div>
+      <form className="form" onSubmit={searchPhotos}>
+              <label className="label" htmlFor="query"> 
+        {" "}
+        📷
+      </label>
+      <input
+        type="text"
+        name="query"
+        className="input"
+        placeholder={`Try anything related to space`}
+        value={query}
+    onChange={(e) => setQuery(e.target.value)}
+      />
+      <button type="submit" className="button">
+        Search
+      </button>
+    </form>
+    <div className="card-list">
+        {pics.map((pic) =>
+          <div className="card" key={pic.id}>
+            <img
+              className="card--image"
+              alt={pic.alt_description}
+              src={pic.urls.full}
+              width="50%"
+              height="50%"
+            ></img>
+          </div>)}
+      </div>
+    </>
+  );
 }
 
 export default Search;
